9268ce9183882cf44affceea94ace6a8f2799927
Emad Baqeri <ebaqeri@protonmail.com>
2023-03-13 12:40:13 +0330

    add prettier to the scripts

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Pressable, Text } from 'react-native';
+import {Pressable, Text} from 'react-native';
 import type {PressableProps} from 'react-native';
 
 import styles from './button.styles';
 
-type ButtonProps = {} & PressableProps & React.PropsWithChildren
+type ButtonProps = {} & PressableProps & React.PropsWithChildren;
 
-const Button = ({ children, ...props }: ButtonProps) => {
-
+const Button = ({children, ...props}: ButtonProps) => {
   if (typeof children === 'string') {
-    children = <Text>{children}</Text>
+    children = <Text>{children}</Text>;
   }
 
-  return <Pressable style={styles.container} {...props}>{children}</Pressable>;
+  return (
+    <Pressable style={styles.container} {...props}>
+      {children}
+    </Pressable>
+  );
 };
 
 export default Button;
